2542d304068fdeb35124678b8edfce3464cd270c
Gayatri Mishra <gayatrimishra2424@gmail.com>
2025-11-06 12:07:21 +0530

    Fixed Tailwind PostCSS config and restore functional build

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,7 +1,7 @@
 import { ArrowDown } from "lucide-react"
 
 export const HomeSection = () =>{
-    return <section id = "home" className="relative min-h-screen flex flex-col items-center justify-center px-4">\
+    return <section id = "home" className="relative min-h-screen flex flex-col items-center justify-center px-4">
     
     <div className="max-w-4xl mx-auto text-center z-10"> 
         <div className="space-y-6">
